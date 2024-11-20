198dcaddd904c42bfd0f78ff31c845373c67a8b4
IkerBe48 <ikerbm@msmk.university>
2024-11-20 20:35:59 +0100

    Creacion de clases Cita y Medico e interfaz por consola

--- Gestion_Hospitalaria/Gestion_Hospitalaria/Gestion_Hospitalaria.cpp
+++ Gestion_Hospitalaria/Gestion_Hospitalaria/Gestion_Hospitalaria.cpp
@@ -2,6 +2,7 @@
 //
 
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -21,9 +22,43 @@ public:
     string especialidad;
     bool disponible;
 };
+// Clase Cita
+class Cita {
+public:
+    unique_ptr<Paciente> paciente;
+    unique_ptr<Medico> medico;
+    string fecha;
+    int urgencia; // 1: baja, 2: media, 3: alta
+
+    Cita(Paciente* p, Medico* m, string fecha, int urgencia)
+        : paciente(p), medico(m), fecha(fecha), urgencia(urgencia) {
+    }
+};
+
+// Clase Hospital
+class Hospital {
+private:
+    vector<unique_ptr<Paciente>> pacientes;
+    vector<unique_ptr<Medico>> medicos;
+    vector<unique_ptr<Cita>> citas;
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int opcion;
+    while (true) {
+        cout << "------Menu------\n";
+        cout << "1. Salir\n";
+        cin >> opcion;
+
+        switch (opcion) {
+            case 1:
+                return 0;
+            default:
+                cout << "Opcion invalida. Intente de nuevo.\n";
+        }
+    }
+
+    return 0;
 }
 
